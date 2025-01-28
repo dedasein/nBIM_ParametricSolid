d3b2d51ac554a0e82c5d2282be6b0847a8f9b734
dedasein <ded_dasein@mail.ru>
2025-01-28 12:13:24 +0700

    Добавлены grips изменения длин

--- nBIM_ParametricSolid/Class1.cs
+++ nBIM_ParametricSolid/Class1.cs
@@ -48,16 +48,33 @@ namespace nBIM_ParametricSolid
             //Добавление в геометрию примитива - параллелепипед
             box.AddParameter("Length", "", "Длина параллелепипеда", "[DIM_LENGTH]");
             box.AddParameter("Width", "", "Ширина параллелепипеда", "[DIM_WIDTH]");
-            box.AddParameter("Height", "", "Высота параллелепипеда", "[DIM_WIDTH]");
+            box.AddParameter("Height", "", "Высота параллелепипеда", "[DIM_HEIGHT]");
             //Если добавляемых элементов несколько, либо присутствуют вспомогательные элементы, можно воспользоваться методом
             //AddGroupToSubEntity, группирующим несколько элементов и позволяющим управлять группой элементов, как единым примитивом. 
 
 
-            //В созданный примитив нужно добавить необходимые ему параметры, стандартный набор для 3д примитива.
+            //В созданный примитив нужно добавить необходимые ему параметры, стандартный набор для 3д примитива (НАЙТИ ИНФУ ЧТО ЭТО)
             box.AddSubEntityMainParams(true, true, false);
 
             //Обновляем геометрию объекта после добавления свойств
             entity.UpdateElements();
+
+            //Добавление ручек управления
+            //GetGripData() - возвращает информацию о ручках для редактирования геометрии
+            //var boxgrips = entity.GetGripData(ViewMode.Model3D);
+
+            //Добавляем грип длины, вручную собирая свойства
+            var gripLength = entity.AddGrip("LENGTH", "Длина", "DIM_LENGTH");
+            gripLength.SetSplitParameter("Direction", new Vector3d(1, 0, 0));
+            gripLength.SetSplitParameter("Orientation", new Vector3d(0, 0, 1));
+
+            var gripWidth = entity.AddGrip("LENGTH", "Ширина", "DIM_WIDTH");
+            gripWidth.SetSplitParameter("Direction", new Vector3d(0, 1, 0));
+            gripWidth.SetSplitParameter("Orientation", new Vector3d(0, 0, 1));
+
+            var gripHeight = entity.AddGrip("LENGTH", "Высота", "DIM_HEIGHT");
+            gripHeight.SetSplitParameter("Direction", new Vector3d(0, 0, 1));
+            gripHeight.SetSplitParameter("Orientation", new Vector3d(1, 0, 0));
 
             //Добавление объекта в модель
             //Transaction - транзакции помогают расширить спектр связанных с объектом операций и избежать нежелательных конфликтных ситуаций.
